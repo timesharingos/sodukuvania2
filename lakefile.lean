81d1c33d0837021ec51f5bf07bf9e0a3a51dca4f
6607changchun <luobojiaozi@163.com>
2026-09-21 19:45:22 +0800

    chore: init

---
+++ lakefile.lean
@@ -0,0 +1,18 @@
+import Lake
+open Lake DSL
+
+package "sodukuvania2" where
+  version := v!"0.1.0"
+  keywords := #["math"]
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
+    ⟨`relaxedAutoImplicit, false⟩,
+    ⟨`maxSynthPendingDepth, .ofNat 3⟩,
+    ⟨`weak.linter.mathlibStandardSet, true⟩,
+  ]
+
+require "leanprover-community" / "mathlib" @ git "v4.34.0"
+
+@[default_target]
+lean_lib «Sodukuvania2» where
+  -- add any library configuration options here
